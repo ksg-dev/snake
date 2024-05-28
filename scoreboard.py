96566079a6eb425ee7b003ca4e9857c4582fb919
ksg-dev <ksg.dev.data@gmail.com>
2024-05-28 14:38:00 -0500

    scoreboard function

---
+++ scoreboard.py
@@ -0,0 +1,30 @@
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = ('Courier', 15, 'normal')
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.penup()
+        self.goto(0,250)
+        self.color("yellow")
+        self.score = 0
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def point(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
+
+
+
+
+
+
+
